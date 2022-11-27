bbeffb71a4036333fdc11ec60b2b93c676740209
Tyler Stambaugh <11870675+tylerstambaugh@users.noreply.github.com>
2022-11-27 17:47:03 -0500

    Add project files.

---
+++ HyperOptimizedTelemetry/Program.cs
@@ -0,0 +1,15 @@
+﻿
+// https://exercism.org/tracks/csharp/exercises/hyper-optimized-telemetry/edit
+
+public static class TelemetryBuffer
+{
+    public static byte[] ToBuffer(long reading)
+    {
+        throw new NotImplementedException("Please implement the static TelemetryBuffer.ToBuffer() method");
+    }
+
+    public static long FromBuffer(byte[] buffer)
+    {
+        throw new NotImplementedException("Please implement the static TelemetryBuffer.FromBuffer() method");
+    }
+}
